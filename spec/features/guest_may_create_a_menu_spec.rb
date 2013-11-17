f98a62ac9e62bc5e6a4596860a33f26ba877c5b5
Apprentice <apprentice@dbc05.local>
2013-11-17 12:47:33 -0800

    List the menus below the form and bug-fix my capybara expect to not explicitly have "Menu" in the name

--- spec/features/guest_may_create_a_menu_spec.rb
+++ spec/features/guest_may_create_a_menu_spec.rb
@@ -1,12 +1,3 @@
-# This is failing on: 
-
-# ArgumentError:
-#   rack-test requires a rack application, but none was given
-
-# This comes from the ENV and complex for me to debug right now
-
-# ToDo: What is rack-test, why require rack app, how capybara runs
-
 require 'spec_helper'
 
 feature "Creating a menu" do
@@ -15,6 +6,7 @@ feature "Creating a menu" do
     fill_in "name", with: "Yumilicious Dinner"
     click_on "Add Menu"
 
-    expect(page).to have_content("Yumilicious Dinner Menu")
+    expect(page).to have_content("Yumilicious Dinner")
   end
 end
+
