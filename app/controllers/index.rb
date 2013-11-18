6d64b4088ac2d3873cbf87e2613a5c52bec87393
Apprentice <apprentice@dbc05.local>
2013-11-18 09:00:53 -0800

    stash

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -36,21 +36,11 @@ end
 
 post '/menus/items' do
   menu_name = params[:menu_name]
-  item_id = params[:item_id]
   the_menu_record = Menu.where(name: menu_name)
   @menu_id = the_menu_record[0].id
-  the_item_record = Item.where(id: item_id)
+  the_item_record = Item.find_by_id(params[:item_id])
   the_menu_record[0].items << the_item_record
+  
   redirect "/menus/#{@menu_id}"
 end
-
-# > a_menu = Menu.last
-# > an_item = Item.last
-# > a_menu.items << an_item 
-
-#   # item = find item
-#   # menu = find menu
-#  # associate item with menu
-#    menu.items << item
-# end
 
